cf70bf8beddcdfe1460c307177dcac770ec2d979
KarmanniyMukuta <gen.nefchev@gmail.com>
2024-07-19 16:44:43 +0300

    add useWindowScroll

--- 6-hooks/src/App.js
+++ 6-hooks/src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import Windowscroll from './Windowscroll';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Windowscroll />
       </header>
     </div>
   );
