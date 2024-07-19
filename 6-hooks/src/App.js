9d153460ac487beeab0924ad29cfe26f49d91b31
KarmanniyMukuta <gen.nefchev@gmail.com>
2024-07-19 17:25:19 +0300

    added useWindowsize

--- 6-hooks/src/App.js
+++ 6-hooks/src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import Windowscroll from './Windowscroll';
+import WindowSize from './WindowSize'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Windowscroll />
+        <WindowSize />
       </header>
     </div>
   );
